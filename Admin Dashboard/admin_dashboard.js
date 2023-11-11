4f7953d9b3e8e4afa3bb67142cb29416988da84d
UIT21522336 <21522336@gm.uit.edu.vn>
2023-11-11 15:59:22 +0700

    update admin dashboard

---
+++ Admin Dashboard/admin_dashboard.js
@@ -0,0 +1,18 @@
+const body = document.querySelector("body"),
+        sidebar= body.querySelector(".sidebar-hnm"),
+        toggle= body.querySelector(".toggle-hnm"),
+        modeSwitch= body.querySelector(".toggle-switch-hnm"),
+        modeText= body.querySelector(".mode-text-hnm");
+
+        toggle.addEventListener("click",()=>{
+            sidebar.classList.toggle("close");
+        })
+        modeSwitch.addEventListener("click",()=>{
+            body.classList.toggle("dark");
+            if(body.classList.contains("dark")){
+                modeText.innerText="Chế độ sáng"
+            }
+            else{
+                modeText.innerText="Chế độ tối"
+            }
+        });
